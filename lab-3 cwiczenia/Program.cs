60ae10ab7fa350c44de0e576e4d75686090ef09e
Acer Nitro 5 <Acer Nitro 5@DESKTOP-N97QLHD>
2022-03-22 23:28:52 +0100

    end

--- lab-3 cwiczenia/Program.cs
+++ lab-3 cwiczenia/Program.cs
@@ -6,7 +6,89 @@ namespace lab_3_cwiczenia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Container<Bottle> a = new(3);
+            Bottle butelka =new("Naleczowianka");
+            Bottle butelka2 =new("Mineralna");
+            Bottle butelka3 =new("Zywiec zdroj");
+            a.dodanie(butelka);
+            a.dodanie(butelka2);
+            a.dodanie(butelka3);
+            Console.WriteLine(a._array[2].ToString());
+            Console.WriteLine(a.iloscWSrodku());
+            a.usuniecie(2);
+            Console.WriteLine(a.iloscWSrodku());
+        }
+    }
+    public class Container<T> where T : Bottle,new()
+    {
+        internal T[] _array;
+        private int Wielkosc;
+
+        public Container( int wielkosc)
+        {
+            _array = new T[wielkosc];
+            Wielkosc = wielkosc;
+        }
+
+        public void dodanie(T value)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] == null)
+                {
+                    _array[i] = value;
+                    break;
+                }
+            }
+
+        }
+        public int iloscWSrodku()
+        {
+            int ilosc = 0;
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] == null)
+                {
+                    break;
+                }
+                ilosc++;
+            }
+            return ilosc;
+        }
+        public void usuniecie(int index)
+        {
+            int ilosc = 0;
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] != null)
+                {
+                    ilosc++;
+                }
+            }
+            if (ilosc < 0)
+            {
+                Console.WriteLine("Brak elementu do usuniecia");
+                return;
+            }
+            _array[index] = null;
+
+        }
+    }
+    public class Bottle
+    {
+        public string name;
+
+        public Bottle(string name)
+        {
+            this.name = name;
+        }
+        public Bottle()
+        {
+            
+        }
+        public override string ToString()
+        {
+            return name;
         }
     }
 }
