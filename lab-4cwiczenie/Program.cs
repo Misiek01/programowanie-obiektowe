d6d96d7e5f99c183725ff1f3a0a4e39d3b369ba0
Acer Nitro 5 <Acer Nitro 5@DESKTOP-N97QLHD>
2022-04-05 21:43:08 +0200

    koniec

--- lab-4cwiczenie/Program.cs
+++ lab-4cwiczenie/Program.cs
@@ -31,6 +31,59 @@ namespace lab_4cwiczenie
             Console.WriteLine( Exercise1.NextPoint(Direction4.UP, (2, 3), (5, 5)));
         }
     }
+    record Student(string LastName, string FirstName, char Group, string StudentId = "");
+    class Exercise4
+    {
+        public static void AssignStudentId(Student[] students)
+        {
+            string stID = "";
+            int A = 1;
+            int B = 1;
+            int C = 1;
+            for (int i = 0; i < students.Length; i++)
+            {
+                stID += students[i].Group;
+                stID += students[i] switch
+                {
+                    { Group: 'A' } => A++.ToString("D3"),
+                    { Group: 'B' } => B++.ToString("D3"),
+                    { Group: 'C' } => C++.ToString("D3"),
+                    _ => -1
+                };
+                students[i] = students[i] with { StudentId = stID };
+                stID = "";
+
+            }
+        }
+    }
+    enum Direction4
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+    class Exercise1
+    {
+        public static (int, int) NextPoint(Direction4 direction, (int, int) point, (int, int) screenSize)
+        {
+            var krotka = point;
+            krotka = direction switch
+            {
+                Direction4.UP => (krotka.Item1, krotka.Item2 -= 1),
+                Direction4.DOWN => (krotka.Item1, krotka.Item2 += 1),
+                Direction4.LEFT => (krotka.Item1 -= 1, krotka.Item2),
+                Direction4.RIGHT => (krotka.Item1 += 1, krotka.Item2),
+                _ => (krotka.Item1, krotka.Item2)
+            };
+            if (krotka.Item1 > screenSize.Item1 || krotka.Item2 > screenSize.Item2 ||
+                krotka.Item1 < 0 || krotka.Item2 < 0)
+            {
+                return point;
+            }
+            return krotka;
+        }
+    }
     record Car(string Model = "Audi", bool HasPlateNumber = false, int Power = 100);
 
 
@@ -55,4 +108,5 @@ namespace lab_4cwiczenie
             return ilosc;
         }
     }
+
 }
